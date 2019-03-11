2643111aab2ade5be228fa96a6b9fdcd0d3adea6
blackyau <blackyau426@gmail.com>
2019-03-11 17:00:19 +0800

    Add 2013 Java C 1

---
+++ GuessAge.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+
+//2013 Java C 1
+public class GuessAge {
+	public static void main(String[] args) {
+		int a,b;
+		for(int i=0; i<=100; i++){
+			if(i*i*i>=1000 && i*i*i<=9999){
+				a=i*i*i;
+				if(i*i*i*i>=100000 && i*i*i*i<=999999){
+					b=i*i*i*i;
+					String stra = String.valueOf(a);
+					String strb = String.valueOf(b);
+					HashSet list = new HashSet();
+					for(int c=0; c<=stra.length(); c++){
+						if(!list.add(stra.charAt(c))){
+							break;
+						}else{
+							for(int d=0; c<=strb.length(); d++){
+								if(!list.add(strb.charAt(d))){
+									break;
+								}else{
+									System.out.println(i);
+									System.exit(0);
+								}
+							}
+						}
+					}
+				}
+			}
+			
+		}
+	}
+
+}
--- README.md
+++ README.md
@@ -11,6 +11,20 @@ Virtual Judge [https://vjudge.net/status#un=BlackYau](https://vjudge.net/status#
 - 最长公共子序列
 - 八皇后问题
 
+## 猜年龄
+
+蓝桥杯 2013 Java C 1
+
+美国数学家维纳(N.Wiener)智力早熟，11岁就上了大学。他曾在1935~1936年应邀来中国清华大学讲学。一次，他参加某个重要会议，年轻的脸孔引人注目。于是有人询问他的年龄，他回答说：
+
+“我年龄的立方是个4位数。我年龄的4次方是个6位数。这10个数字正好包含了从0到9这10个数字，每个都恰好出现1次。”
+
+请你推算一下，他当时到底有多年轻。
+
+> 使用 HashSet 实现了检测每一位是否重复，暴力枚举每一位数。
+
+完整代码:[https://github.com/blackyau/homework/blob/master/GuessAge.java](https://github.com/blackyau/homework/blob/master/GuessAge.java)
+
 ## UVa 514 铁轨
 
 [https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=455](https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=455)
